d3f6e8e42fae5092416420f16b2ad57367b4d133
Yong Jun Xi <89063663+TobyCyan@users.noreply.github.com>
2024-07-18 15:48:19 +0800

    Delete Website Tutorial directory

--- Website Tutorial/main.py
+++
@@ -1,7 +0,0 @@
-from website._init_ import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    # runs application with debug mode on
-    app.run(debug = True)
--- Website Tutorial/tempCodeRunnerFile.javascript
+++
@@ -1,9 +0,0 @@
-const quiz = () => {
-    let x = 3;
-    let y = 5;
-    let z = 10;
-    let c = ++z + y - y + z + x++;
-    console.log(c);
-}
-
-quiz();
