540d845b9dc201c26a5531b9c831241ccc1e28f6
Yong Jun Xi <ernestyongjunxi@gmail.com>
2024-07-29 05:01:10 +0800

    Fixed isTodayBirthday Check

--- __tests__/dev2.test.js
+++ __tests__/dev2.test.js
@@ -1,33 +1,38 @@
 import {checkStrongPW} from "../client/src/utilities/utilities";
+import { isTodayBirthday } from "../client/src/utilities/utilities";
 
 // Unit Tests to test for Strong Password Checker.
-const pw1 = '123456789'
-const pw2 = 'es2549'
-const pw3 = 'H123sw'
-const pw4 = 'H123sw5GQa'
-const pw5 = '159756jkQ'
-const pw6 = '9lqsncO'
+// const pw1 = '123456789'
+// const pw2 = 'es2549'
+// const pw3 = 'H123sw'
+// const pw4 = 'H123sw5GQa'
+// const pw5 = '159756jkQ'
+// const pw6 = '9lqsncO'
 
-test('tests the strong pw checker 1', () => {
-    expect(checkStrongPW(pw1)).toEqual(false)
-})
+// test('tests the strong pw checker 1', () => {
+//     expect(checkStrongPW(pw1)).toEqual(false)
+// })
 
-test('tests the strong pw checker 2', () => {
-    expect(checkStrongPW(pw2)).toEqual(false)
-})
+// test('tests the strong pw checker 2', () => {
+//     expect(checkStrongPW(pw2)).toEqual(false)
+// })
 
-test('tests the strong pw checker 3', () => {
-    expect(checkStrongPW(pw3)).toEqual(false)
-})
+// test('tests the strong pw checker 3', () => {
+//     expect(checkStrongPW(pw3)).toEqual(false)
+// })
 
-test('tests the strong pw checker 4', () => {
-    expect(checkStrongPW(pw4)).toEqual(true)
-})
+// test('tests the strong pw checker 4', () => {
+//     expect(checkStrongPW(pw4)).toEqual(true)
+// })
 
-test('tests the strong pw checker 5', () => {
-    expect(checkStrongPW(pw5)).toEqual(true)
-})
+// test('tests the strong pw checker 5', () => {
+//     expect(checkStrongPW(pw5)).toEqual(true)
+// })
 
-test('tests the strong pw checker 6', () => {
-    expect(checkStrongPW(pw6)).toEqual(false)
+// test('tests the strong pw checker 6', () => {
+//     expect(checkStrongPW(pw6)).toEqual(false)
+// })
+const bday1 = "2003-07-29T00:00:00"
+test('tests the today birthday checker', () => {
+        expect(isTodayBirthday(bday1)).toEqual(true)
 })
